e1d304ff03306da66dbad766486e1d2ccb5bfadb
Fedor <fdr@mail.ru>
2024-08-05 10:38:36 +0300

    hw1

---
+++ InMemoryModel/ModelStore.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+
+public class ModelStore implements IModelChangeObserver, IModelChanger {
+
+  ArrayList<PoligonalModel> Models = new ArrayList<>();
+  ArrayList<Scene> Scenes = new ArrayList<>();
+  ArrayList<Flash> Flashes = new ArrayList<>();
+  ArrayList<Camera> Cameras = new ArrayList<>();
+
+  public Scene getScene(int n_scene) {
+
+    return Scenes.get(n_scene);
+  }
+
+  @Override
+  public void NotifyChange() {
+
+  }
+
+  @Override
+  public void ApplyUpdateModel() {
+
+  }
+}
